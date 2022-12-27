8b142d4e32046982b8719ddc23bdd38a70dd56ad
Budddy <budddyjellz@gmail.com>
2022-12-27 10:54:51 -0500

    Player Image Moving on Screen

---
+++ src/org/budddy/entity/Entity.java
@@ -0,0 +1,13 @@
+package org.budddy.entity;
+
+import java.awt.image.BufferedImage;
+
+public class Entity {
+
+    public int x, y;
+    public int speed;
+
+    public BufferedImage upl, upr, ups, downl, downr, downs, leftl, leftr, lefts, rightl, rightr, rights;
+    public String direction;
+    public int random, tempRandom;
+}
---
+++ src/org/budddy/entity/Player.java
@@ -0,0 +1,145 @@
+package org.budddy.entity;
+
+import org.budddy.main.GamePanel;
+import org.budddy.main.KeyHandler;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class Player extends Entity{
+
+    GamePanel gp;
+    KeyHandler keyH;
+
+    public Player(GamePanel gp, KeyHandler keyH) {
+        this.gp = gp;
+        this.keyH = keyH;
+
+        setDefaultValues();
+        getPlayerImage();
+    }
+
+    public void setDefaultValues() {
+
+        x = 100;
+        y = 100;
+        speed = 4;
+        direction = "down";
+        random = -1;
+        tempRandom = -1;
+
+    }
+
+    public void getPlayerImage() {
+
+        try {
+
+            upl = ImageIO.read(getClass().getResourceAsStream("/player/up-l.png"));
+            upr = ImageIO.read(getClass().getResourceAsStream("/player/up-r.png"));
+            ups = ImageIO.read(getClass().getResourceAsStream("/player/up-s.png"));
+            downl = ImageIO.read(getClass().getResourceAsStream("/player/down-l.png"));
+            downr = ImageIO.read(getClass().getResourceAsStream("/player/down-r.png"));
+            downs = ImageIO.read(getClass().getResourceAsStream("/player/down-s.png"));
+            leftl = ImageIO.read(getClass().getResourceAsStream("/player/left-l.png"));
+            leftr = ImageIO.read(getClass().getResourceAsStream("/player/left-r.png"));
+            lefts = ImageIO.read(getClass().getResourceAsStream("/player/left-s.png"));
+            rightl = ImageIO.read(getClass().getResourceAsStream("/player/right-l.png"));
+            rightr = ImageIO.read(getClass().getResourceAsStream("/player/right-r.png"));
+            rights = ImageIO.read(getClass().getResourceAsStream("/player/right-s.png"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void update() {
+
+        if (keyH.upPressed) {
+            direction = "up";
+            random = (int) Math.floor(Math.random() * 2);
+            y -= speed;
+        }
+        else if (keyH.downPressed) {
+            direction = "down";
+            random = (int) Math.floor(Math.random() * 2);
+            y += speed;
+        }
+        else if (keyH.leftPressed) {
+            direction = "left";
+            random = (int) Math.floor(Math.random() * 2);
+            x -= speed;
+        }
+        else if (keyH.rightPressed) {
+            direction = "right";
+            random = (int) Math.floor(Math.random() * 2);
+            x += speed;
+        }
+        else {
+            random = -1;
+            tempRandom = -1;
+        }
+    }
+    public void draw(Graphics2D g2) {
+
+//        g2.setColor(Color.white);
+//        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+
+
+        BufferedImage image = null;
+
+        if (random == 0) {
+            image = upl;
+        } else if (random == 1) {
+            image = upr;
+        }
+
+        switch (direction) {
+            case "up":
+                if (random == 0) {
+                    image = upl;
+                } else if (random == 1) {
+                    image = upr;
+                }
+                if (random == -1) {
+                    image = ups;
+                }
+                break;
+
+            case "down":
+                if (random == 0) {
+                    image = downl;
+                } else if (random == 1) {
+                    image = downr;
+                } else if (random == -1) {
+                    image = downs;
+                }
+                break;
+
+            case "left":
+                if (random == 0) {
+                    image = leftl;
+                } else if (random == 1) {
+                    image = leftr;
+                } else if (random == -1) {
+                    image = lefts;
+                }
+                break;
+
+
+            case "right":
+                if (random == 0) {
+                    image = rightl;
+                } else if (random == 1) {
+                    image = rightr;
+                } else if (random == -1) {
+                    image = rights;
+                }
+                break;
+        }
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+
+    }
+}
